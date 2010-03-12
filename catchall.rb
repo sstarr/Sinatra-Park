ca6842a25e48fde284351e09d083131523b33dad
Simon Starr <simon@starr.cx>
2010-03-12 17:15:52 +0000

    I might have just broken this completely.

--- catchall.rb
+++ catchall.rb
@@ -1,38 +1,20 @@
 require 'rubygems'
 require 'sinatra'
-require 'haml'
+require 'erb'
 
-Domains = %w{linkdeleted.com allmouthandnotrousers.com evans.cx pimpdog.co.uk straydogimages.com straydogimages.co.uk}
-DefaultDomain = 'default'
-
-# what domain does the host contain?
-# put this here because helpers are a pain to test!
-def view_for_domain(host)
-  return DefaultDomain if host.nil?
-  
-  Domains.find do |domain|
-     host.include?(domain)
-  end || DefaultDomain
-end
+Domains = %w{emmawhiting.co.uk}
 
 def view_exists?(view)
-  File.exists?(Dir.pwd + "/views/#{view}.haml")
+  File.exists?(Dir.pwd + "/views/#{view}.erb")
 end
 
 # the catch-all route
 get '/*' do
   # parse domain
-  @view = view_for_domain(request.env["HTTP_HOST"])
-  
-  # override with text anywhere in url (for testing)
-  splat = params['splat'].first 
-  if(@view == DefaultDomain && !splat.empty?) then 
-    @view = view_for_domain(splat)
-  end
-  
   @domain = request.env["HTTP_HOST"].sub(/^(?:www)\./, '')
   
   # ensure view exists, and load
-  @view = DefaultDomain unless view_exists?(@view)
-  haml @view.to_sym
+  @view = @domain
+  @view = 'default' unless view_exists?(@domain)
+  erb @view.to_sym
 end
