c76bcc1c2c0949a51ad401822cdefdb76b1b5cce
sstarr <simon@starr.cx>
2011-06-22 21:19:51 +0100

    Removed a couple of domains.

--- catchall.rb
+++ catchall.rb
@@ -6,13 +6,10 @@ def view_exists?(view)
   File.exists?(Dir.pwd + "/views/#{view}.erb")
 end
 
-# the catch-all route
 get '/*' do
-  # parse domain
   @domain = request.env["HTTP_HOST"].sub(/^(?:www)\./, '')
   
-  # ensure view exists, and load
-  @view = @domain
-  @view = 'default' unless view_exists?(@domain)
-  erb @view.to_sym
+  view = @domain
+  view = 'default' unless view_exists?(@domain)
+  erb view.to_sym
 end
